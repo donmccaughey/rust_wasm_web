07ab7f4f36a148aac5fdbb533bb8405866d1ae49
Don McCaughey <don@donm.cc>
2018-10-28 16:54:26 -0700

    Add some DOM manipulation.
    Change the name of the exported Rust function from `greet()` to `run()`
    and pass in a container ID.
    
    Add the `web_sys` crate to the project and use the browser functions
    defined there, removing the one-off extern definition for `alert()`.
    
    Add console logging, insert DOM nodes and call
    `window.alert_with_message()`.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,23 @@
 extern crate wasm_bindgen;
+extern crate web_sys;
+
+use web_sys::console;
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
-}
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+pub fn run(container_id: &str) -> Result<(), JsValue> {
+    console::log_2(&"run() start, container_id:".into(), &container_id.into());
+
+    let window = web_sys::window().expect("window not found");
+    window.alert_with_message(&format!("Hello, rust_wasm_web!"))?;
+
+    let document = window.document().expect("document not found");
+    let container = document.get_element_by_id(container_id).expect("container not found");
+
+    container.set_inner_html("<p>Hello, <code>rust_wasm_web</code>!</p>");
+
+    console::log_1(&"run() end".into());
+    Ok(())
 }
 
